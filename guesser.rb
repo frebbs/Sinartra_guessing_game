2ecce7a37db71ea4052a0f76829ad34516391b18
Aaron <aaron.diable@gmail.com>
2020-01-06 21:13:14 +0000

    Initial refactor

--- guesser.rb
+++ guesser.rb
@@ -7,26 +7,15 @@ player_wins = 0
 
 get '/' do
   user_guess = params["guess"].to_i
-  game_message = check_user_guess(user_guess)
-
-  if user_guess == rand_num
-    message = "You have won, try to guess the new number!"
+  game_message = check_user_guess(user_guess, rand_num)
+  if user_win(user_guess, rand_num)
     player_wins += 1
-    rand_num = rand(101)
-  elsif user_guess.between?(rand_num, rand_num + 10)
-    message = "Close, but still too high!"
-  elsif user_guess.between?(rand_num - 10, rand_num)
-    message = "Close, but still too low"
-  elsif user_guess > rand_num
-    message = "That guess was way too high!"
-  elsif user_guess < rand_num && user_guess > 0
-    message = "That guess was way too low!"
-  else
-    message = "Please enter a number to begin the game"
+    rand_num = rand(1..100)
   end
 
-  erb :index, :locals => {:rand_num => rand_num, :user_guess => user_guess, :message => game_message,
-                          :win_count => player_wins}
+
+
+  erb :index, :locals => {:user_guess => user_guess, :message => game_message, :win_count => player_wins}
   #throw params.inspect
 end
 
--- helpers/check_user_guess.rb
+++ helpers/check_user_guess.rb
@@ -1,5 +1,25 @@
-def check_user_guess(guess = nil, random_num)
-  if 1 + 1 == 2
-    "This works too"
+def user_win(guess, random_num)
+  if guess == random_num
+    true
+  else
+    false
   end
 end
+
+def check_user_guess(guess, random_num)
+  if user_win(guess, random_num)
+    "You have won, try to guess the new number!"
+  elsif guess.between?(random_num, (random_num + 10))
+    "Close, but still too high!"
+  elsif guess.between?((random_num - 10), random_num)
+    "Close, but still too low"
+  elsif guess > random_num
+    "That guess was way too high!"
+  elsif guess < random_num && guess > 0
+    "That guess was way too low!"
+  else
+    "Please enter a number to begin the game"
+  end
+end
+
+
