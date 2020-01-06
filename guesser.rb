d716af09a1e58e0195e412d7073e986d507abe5f
Aaron <aaron.diable@gmail.com>
2020-01-06 20:04:03 +0000

    Added win counter and some initial game logic

--- guesser.rb
+++ guesser.rb
@@ -2,8 +2,26 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 rand_num = rand(101)
+player_wins = 0
 
 get '/' do
-  erb :index, :locals => {:rand_num => rand_num}
+  user_guess = params["guess"].to_i
+
+
+  if user_guess == rand_num
+    message = "You have won, try to guess the new number!"
+    player_wins += 1
+    rand_num = rand(101)
+  elsif user_guess > rand_num
+    message = "That guess was too high!"
+  elsif user_guess < rand_num && user_guess > 0
+    message = "That guess was too low!"
+  else
+    message = "Please enter a number to begin the game"
+  end
+
+  erb :index, :locals => {:rand_num => rand_num, :user_guess => user_guess, :message => message,
+                          :win_count => player_wins}
+  #throw params.inspect
 end
 
