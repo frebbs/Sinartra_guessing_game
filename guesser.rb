28a4b0b9b747d9f243c6226c07af1bba77517695
Aaron <aaron.diable@gmail.com>
2020-01-06 21:56:32 +0000

    Added cheat mode

--- guesser.rb
+++ guesser.rb
@@ -8,6 +8,12 @@ player_wins = 0
 get '/' do
   user_guess = params["guess"].to_i
   game_message = check_user_guess(user_guess, rand_num)
+  cheat_mode = nil
+
+  if params["cheat"]
+    cheat_mode = rand_num
+  end
+  
   if user_win(user_guess, rand_num)
     player_wins += 1
     rand_num = rand(1..100)
@@ -15,7 +21,8 @@ get '/' do
 
 
 
-  erb :index, :locals => {:user_guess => user_guess, :message => game_message, :win_count => player_wins}
+  erb :index, :locals => {:user_guess => user_guess, :message => game_message, :win_count => player_wins,
+                          :cheat_mode => cheat_mode}
   #throw params.inspect
 end
 
--- helpers/check_user_guess_helpers.rb
+++ helpers/check_user_guess_helpers.rb
@@ -22,4 +22,11 @@ def check_user_guess(guess, random_num)
   end
 end
 
+def cheat_mode_helper(x)
+  if x
+    true
+  else
+    false
+  end
 
+end
