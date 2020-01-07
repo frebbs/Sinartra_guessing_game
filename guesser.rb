23073a406623bea47a0b0ce77635021949a720b9
Aaron Diable <aaron.diable@gmail.com>
2020-01-07 23:25:04 +0000

    All game logic removed from guesser app file in to the game class

--- guesser.rb
+++ guesser.rb
@@ -2,23 +2,12 @@ require 'sinatra'
 require 'sinatra/reloader'
 require './helpers/game_class'
 
-#rand_num = rand(1..101)
-#
-
 game = Game.new
-rand_num = game.random_number
-player_wins = 0
 
 get '/' do
   user_guess = params["guess"].to_i
-  game_message = game.check_user_guess(user_guess, rand_num)
   cheat_mode = game.random_number if params['cheat'] == "on" ? true : nil
-  if game.user_win(user_guess, rand_num)
-    player_wins += 1
-    rand_num = rand(1..101)
-    puts rand_num
-  end
 
-  erb :index, :locals => {:user_guess => user_guess, :message => game_message, :win_count => player_wins,
-                          :cheat_mode => cheat_mode}
+  erb :index, :locals => {:user_guess => user_guess, :message => game.check_user_guess(user_guess),
+                          :win_count => game.win_counter, :cheat_mode => cheat_mode}
 end
--- helpers/game_class.rb
+++ helpers/game_class.rb
@@ -1,21 +1,21 @@
 class Game
-  attr_accessor :random_number, :player_wins, :cheat
-  # Game class still needs to be completed
-  #
+  attr_accessor :random_number, :win_counter
+
   def initialize
     @random_number = rand(1..101)
+    @win_counter = 0
   end
 
-  def check_user_guess(guess, random_num)
-    if user_win(guess, random_num)
+  def check_user_guess(guess)
+    if user_win(guess, @random_number)
       "You have won, try to guess the new number!"
-    elsif guess.between?(random_num, (random_num + 10))
+    elsif guess.between?(@random_number, (@random_number + 10))
       "Close, but still too high!"
-    elsif guess.between?((random_num - 10), random_num)
+    elsif guess.between?((@random_number - 10), @random_number)
       "Close, but still too low"
-    elsif guess > random_num
+    elsif guess > @random_number
       "That guess was way too high!"
-    elsif guess < random_num && guess > 0
+    elsif guess < @random_number && guess > 0
       "That guess was way too low!"
     else
       "Please enter a number to begin the game"
@@ -24,6 +24,8 @@ class Game
 
   def user_win(guess, random_num)
     if guess == random_num
+      self.win_counter += 1
+      @random_number = rand(1..101)
       true
     else
       false
