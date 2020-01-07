fdbe4f3e985d70ec6a31d26d9a14a23ca737281f
Aaron Diable <aaron.diable@gmail.com>
2020-01-07 17:48:20 +0000

    Change helpers to a game class, work still needs to be done to remove code from routes file and place it in the game class

--- guesser.rb
+++ guesser.rb
@@ -1,25 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require './helpers/check_user_guess_helpers'
+require './helpers/game_class'
 
 rand_num = rand(1..101)
 player_wins = 0
+game = Game.new
 
 get '/' do
   user_guess = params["guess"].to_i
-  game_message = check_user_guess(user_guess, rand_num)
+  game_message = game.check_user_guess(user_guess, rand_num)
   cheat_mode = nil
 
   if params["cheat"]
     cheat_mode = rand_num
   end
 
-  if user_win(user_guess, rand_num)
+  if game.user_win(user_guess, rand_num)
     player_wins += 1
     rand_num = rand(1..100)
   end
 
   erb :index, :locals => {:user_guess => user_guess, :message => game_message, :win_count => player_wins,
                           :cheat_mode => cheat_mode}
+
 end
 
+get '/cats' do
+  erb :cats
+end
---
+++ helpers/game_class.rb
@@ -0,0 +1,32 @@
+class Game
+  attr_reader :random_number
+  # Game class still needs to be completed
+  #
+  def initialize
+    @random_number = rand(1..101)
+  end
+
+  def check_user_guess(guess, random_num)
+    if user_win(guess, random_num)
+      "You have won, try to guess the new number!"
+    elsif guess.between?(random_num, (random_num + 10))
+      "Close, but still too high!"
+    elsif guess.between?((random_num - 10), random_num)
+      "Close, but still too low"
+    elsif guess > random_num
+      "That guess was way too high!"
+    elsif guess < random_num && guess > 0
+      "That guess was way too low!"
+    else
+      "Please enter a number to begin the game"
+    end
+  end
+
+  def user_win(guess, random_num)
+    if guess == random_num
+      true
+    else
+      false
+    end
+  end
+end
